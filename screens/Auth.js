568005cf1dc1de3b1e0bdf505e06a1bdb192d99f
paranoia1906 <anthonymledesma@gmail.com>
2019-01-09 14:17:27 -0700

    Added signout option on protected Auth page which removes JWT from app. Then push navigation to home.

--- screens/Auth.js
+++ screens/Auth.js
@@ -1,21 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { withAuthenticator } from 'aws-amplify-react-native';
+import { Auth } from 'aws-amplify';
 
 class AuthScreen extends React.Component {
     static navigationOptions = {
         title: 'Auth',
       };
+
+      signOutClick() {
+
+      }
+    
     render() {
         const {navigate} = this.props.navigation;
       return (
         <View style={styles.container}>
           <Text>We are now authed!</Text>
+          <Button
+                    title="Sign Out"
+                    onPress={() => {
+                      Auth.signOut();
+                      if (Auth.currentAuthenticatedUser()) {
+                        console.log("Signed out. Pushing to Home.")
+                        this.props.navigation.navigate('Home');
+                      }}}/>
         </View>
       );
     }
   }
-
   
   const styles = StyleSheet.create({
     container: {
